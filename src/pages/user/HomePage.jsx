2ee517d477a8ec5e8ec8c8edc04c2c6f775522bc
FARTUSHNYI OLEKSII <fartushnylex69@gmail.com>
2024-06-27 12:27:50 +0300

    added category block with category card

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -1,5 +1,13 @@
 import { Box } from "@mui/material";
+import Category from "components/user/Category/Category";
+import Footer from "components/user/Footer/Footer";
 
 export default function HomePage() {
-  return <Box sx={{ textAlign: "center" }}>Вас вітає WOOD CRAFTS</Box>;
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      Вас вітає WOOD CRAFTS
+      <Category />
+      <Footer />
+    </Box>
+  );
 }
